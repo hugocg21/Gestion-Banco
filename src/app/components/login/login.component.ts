ca054e671c7515575461ece20692d73a83a4b8de
hugocg21 <hugocastanogonzalez@gmail.com>
2024-07-10 04:29:37 +0200

    Initial commit for the "login" component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe(
+      () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error => {
+        this.errorMessage = 'Usuario o contraseña incorrectos';
+      }
+    );
+  }
+}
