03c9797d0e809a81c8405d8df2d52d1327dd68fc
hugocg21 <hugocastanogonzalez@gmail.com>
2024-07-29 23:29:36 +0200

    Updated the "dashboard" component to display two charts via a menu: one for income and expenses, and another for expense distribution

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,108 +1,142 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { TransactionsService } from '../../services/transactions.service';
-import { BudgetService } from '../../services/budget.service';
 import { Chart, registerables } from 'chart.js';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit {
-  income: number = 0;
-  expenses: number = 0;
-  totalBudget: number = 0;
-  balance: number = 0;
+export class DashboardComponent implements OnInit, AfterViewInit {
+  monthlyIncome: number[] = [];
+  monthlyExpenses: number[] = [];
+  annualExpensesByCategory: { [category: string]: number } = {};
+  months: string[] = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
 
-  constructor(
-    private transactionsService: TransactionsService,
-    private budgetService: BudgetService
-  ) { }
+  @ViewChild('financialSummaryChart') financialSummaryChart!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('expenseDistributionChart') expenseDistributionChart!: ElementRef<HTMLCanvasElement>;
+
+  constructor(private transactionsService: TransactionsService) {}
 
   ngOnInit(): void {
-    this.updateDashboard();
-  }
-
-  updateDashboard() {
-    this.transactionsService.getTotals().subscribe(totals => {
-      this.income = totals.income;
-      this.expenses = totals.expense;
-      this.totalBudget = this.budgetService.getBudgets().reduce((acc, b) => acc + b.amount, 0);
-      this.balance = this.income - this.expenses;
-      this.createIncomeExpenseChart();
-    });
-
-    this.transactionsService.getTotalsByCategory().subscribe(totalsByCategory => {
-      this.createExpenseDistributionChart(totalsByCategory);
+    this.updateSummary();
+    this.transactionsService.getAnnualExpensesByCategory().subscribe(data => {
+      this.annualExpensesByCategory = data;
     });
   }
 
-  createIncomeExpenseChart() {
-    const ctx = (document.getElementById('incomeExpenseChart') as HTMLCanvasElement).getContext('2d');
+  ngAfterViewInit(): void {
+    this.createChart(this.financialSummaryChart.nativeElement, 'financialSummaryChart');
+  }
+
+  updateSummary() {
+    this.transactionsService.getAnnualIncomeExpense().subscribe((data) => {
+      this.monthlyIncome = data.map((item) => item.income);
+      this.monthlyExpenses = data.map((item) => item.expense);
+      this.createChart(this.financialSummaryChart.nativeElement, 'financialSummaryChart');
+    });
+  }
+
+  onTabChange(event: MatTabChangeEvent): void {
+    const tabIndex = event.index;
+    switch (tabIndex) {
+      case 0:
+        this.createChart(this.financialSummaryChart.nativeElement, 'financialSummaryChart');
+        break;
+      case 1:
+        this.createExpenseDistributionChart(this.expenseDistributionChart.nativeElement);
+        break;
+    }
+  }
+
+  createChart(canvas: HTMLCanvasElement, chartId: string) {
+    const ctx = canvas.getContext('2d');
     if (ctx) {
       new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ['Ingresos', 'Gastos'],
-          datasets: [{
-            label: 'Monto',
-            data: [this.income, this.expenses],
-            backgroundColor: [
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-              'rgba(75, 192, 192, 1)',
-              'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 1
-          }]
+          labels: this.months,
+          datasets: [
+            {
+              label: 'Ingresos',
+              data: this.monthlyIncome,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1,
+            },
+            {
+              label: 'Gastos',
+              data: this.monthlyExpenses,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+            },
+          ],
         },
         options: {
+          responsive: true,
+          maintainAspectRatio: false,
           scales: {
             y: {
-              beginAtZero: true
-            }
-          }
-        }
+              beginAtZero: true,
+            },
+          },
+        },
       });
     }
   }
 
-  createExpenseDistributionChart(totalsByCategory: { [category: string]: number }) {
-    const ctx = (document.getElementById('expenseDistributionChart') as HTMLCanvasElement).getContext('2d');
+  createExpenseDistributionChart(canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext('2d');
     if (ctx) {
       new Chart(ctx, {
         type: 'pie',
         data: {
-          labels: Object.keys(totalsByCategory),
-          datasets: [{
-            label: 'Gastos por Categoría',
-            data: Object.values(totalsByCategory),
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
+          labels: Object.keys(this.annualExpensesByCategory),
+          datasets: [
+            {
+              label: 'Gastos por Categoría',
+              data: Object.values(this.annualExpensesByCategory),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
         },
         options: {
-          responsive: true
-        }
+          responsive: true,
+          maintainAspectRatio: false,
+        },
       });
     }
   }
