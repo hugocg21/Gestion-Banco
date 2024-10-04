77e9c4602f17c9178f354ca425411699ac339700
hugocg21 <hugocastanogonzalez@gmail.com>
2024-10-04 18:38:15 +0200

    Header updated after deleting the login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,30 +7,17 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   showNotifications = false;
-  showLogin = false;
 
   ngOnInit(): void {}
 
   toggleNotifications(event: Event) {
     event.stopPropagation();
     this.showNotifications = !this.showNotifications;
-    if (this.showNotifications) {
-      this.showLogin = false;
-    }
-  }
-
-  toggleLogin(event: Event) {
-    event.stopPropagation();
-    this.showLogin = !this.showLogin;
-    if (this.showLogin) {
-      this.showNotifications = false;
-    }
   }
 
   @HostListener('document:click', ['$event'])
   onDocumentClick() {
     this.showNotifications = false;
-    this.showLogin = false;
   }
 
   @HostListener('click', ['$event'])
